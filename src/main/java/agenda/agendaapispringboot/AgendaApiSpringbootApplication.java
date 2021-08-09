1fa11a4ef6ced097da942e90d5404aabe52f75fe
Diego Lima <diiserras@gmail.com>
2021-08-09 17:31:44 -0300

    Initial commit - Create Project

---
+++ src/main/java/agenda/agendaapispringboot/AgendaApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package agenda.agendaapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApiSpringbootApplication.class, args);
+	}
+
+}
